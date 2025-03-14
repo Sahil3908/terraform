119322171b298f7b3450f47d3b72cc0120592f84
Sahil Milind Chaudhari <sahilchaudhari3908@gmail.com>
2025-03-14 09:40:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
-
+pipeline {
+  agent any
+  environment {
+    TF_VAR_key = credentials('terraform-key')
+   }
+   stages {
+   stage('Checkout') {
+    steps {
+     git 'https://github.com/your-username/terraform-repo.git'
+    }
+  }
+   stage('Terraform Init') {
+    steps {
+     sh 'terraform init'
+    }
+  }
+  stage('Terraform Apply') {
+   steps {
+    sh 'terraform apply -auto-approve'
+   }
+  }
+ }
+}
